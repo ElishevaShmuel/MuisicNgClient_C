f1c7da99dbe30a27061b7b684de61f17bfd9e915
ElishevaShmuel <elishevas2004@gmail.com>
2025-05-30 16:30:23 +0300

    dd

--- src/services/Auth/auth.service.ts
+++ src/services/Auth/auth.service.ts
@@ -11,25 +11,29 @@ import { environment } from '../../environments/environments';
 export class AuthService {
   private http = inject(HttpClient);
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
-  private apiUrl = environment.API_URL; 
+  private apiUrl = environment.API_URL;
 
   isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
 
-   async login(credentials: { email: string; password: string }): Promise<Observable<any>> {
+  async login(credentials: { email: string; password: string }): Promise<Observable<any>> {
     console.log('Login ');
 
     return await this.http.post(`${this.apiUrl}/user/admin/login`, credentials).pipe(
       tap((response: any) => {
-        console.log('Login response:', response); 
-        
-        localStorage.setItem('adminToken', response.token);
+        console.log('Login response:', response);
+        if (typeof window !== 'undefined') {
+          localStorage.setItem('adminToken', response.token);
+        }
         this.isAuthenticatedSubject.next(true);
       })
     );
   }
 
   logout() {
-    localStorage.removeItem('adminToken');
+    if (typeof window !== 'undefined') {
+
+      localStorage.removeItem('adminToken');
+    }
     this.isAuthenticatedSubject.next(false);
   }
 
